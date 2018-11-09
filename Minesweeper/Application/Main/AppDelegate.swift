3ed6a8d9793817e7549d778d0730430cfb215058
Josh Maloney <jmmal@users.noreply.github.com>
2018-11-09 13:50:16 +1030

    Initial commit

--- Minesweeper/Application/Main/AppDelegate.swift
+++ Minesweeper/Application/Main/AppDelegate.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+let themeColor = UIColor(red: 0.01, green: 0.41, blue: 0.22, alpha: 1.0)
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -15,6 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
         // Override point for customization after application launch.
         return true
     }
---
+++ Minesweeper/Models/Tile.swift
@@ -0,0 +1,23 @@
+//
+//  Tile.swift
+//  Minesweeper
+//
+//  Created by Josh Maloney on 7/11/18.
+//  Copyright © 2018 Josh Maloney. All rights reserved.
+//
+
+import Foundation
+
+class Tile {
+    var isShown: Bool
+    var isMarked: Bool
+    var isMine: Bool
+    var surroundingMinesCount: Int
+    
+    init(isMine: Bool, surroundingMinesCount: Int) {
+        self.isShown = false
+        self.isMarked = false
+        self.isMine = isMine
+        self.surroundingMinesCount = surroundingMinesCount
+    }
+}
